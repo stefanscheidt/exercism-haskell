8227d485b5977355e728e0f262e56998a6627b88
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2019-10-26 21:21:19 +0200

    nucleotide count using guards

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -1,23 +1,24 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
-import Control.Arrow ((&&&))
 import Data.Map (Map)
 import Data.Map.Lazy (fromList)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
+counts :: String -> Map Nucleotide Int
+counts xs = fromList
+  [ (A, count 'A')
+  , (C, count 'C')
+  , (G, count 'G')
+  , (T, count 'T')
+  ]
+    where 
+      count x = length $ filter (== x) xs
+
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs =
-  let
-    nucleotides = "ACGT"
-    isValid = all (`elem` nucleotides) xs
-    cnt x = length $ filter (== x) xs 
-    nuc 'A' = A
-    nuc 'C' = C
-    nuc 'G' = G
-    nuc 'T' = T
-    nuc _ = error "invalid char"
-  in
-    if isValid
-      then Right $ fromList $ map (nuc &&& cnt) nucleotides  
-      else Left xs
+nucleotideCounts xs
+  | all (`elem` "ACGT") xs  
+    = Right $ counts xs
+  | otherwise
+    = Left xs
+
